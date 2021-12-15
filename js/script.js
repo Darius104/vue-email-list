664997b3efb7df313b39a6e15d3ebceb0deea5ec
Darius104 <comandarius@gmail.com>
2021-12-15 15:49:18 +0100

    Done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
-Vue.config.devtools = true;
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            emailList: [],
+        
+        },
 
-const app = new Vue({
-    el: '#root',
-    data: {},
-    methods: {}
-});
+        methods: {
+           
+        },
+
+        created: function(){
+            for(let i = 0; i < 10; i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+
+                    // console.log(response)
+                    const result = response.data;
+
+                    const email = result.response;
+                    console.log(email);
+
+                    this.emailList.push(email);
+                });
+            }
+            
+        }
+    }
+)
